ec13889a579079e218e510ccd7ca44af34f32921
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-02-23 10:51:21 -0500

    added todos in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,21 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
-
+--id: INT PRIMARY KEY
+--name: VARCHAR(30)
 );
 
 CREATE TABLE roles (
-
+--id: INT PRIMARY KEY
+--title: VARCHAR(30)
+--salary: DECIMAL
+--department_id: INT
 );
 
 CREATE TABLE employee (
-
+--id: INT PRIMARY KEY
+--first_name: VARCHAR(30)
+--last_name: VARCHAR(30)
+--role_id: INT
+--manager_id: INT
 );
