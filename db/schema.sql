9222ac7578d83cd2d666c4b162fd6c4b6576ab32
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-02-23 10:24:47 -0500

    added db and started tables

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+
+);
+
+CREATE TABLE roles (
+
+);
+
+CREATE TABLE employee (
+
+);
